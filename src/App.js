8cc014abf07c43e3a3d609bb8ce1564336fddac6
Vipa4ka <74112668+Vipa4ka@users.noreply.github.com>
2021-07-29 21:53:57 +0300

    Initialize master

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import ContactForm from "./component/ContactForm";
+import Filter from "./component/Filter";
+import ContactList from "./component/ContactList";
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: "",
+  };
+
+  addContacts = (text, tel) => {
+    const contactsId = uuidv4();
+    const add = {
+      id: contactsId,
+      name: text,
+      number: tel,
+    };
+
+    if (this.state.contacts.find((contact) => contact.name === add.name)) {
+      alert(`${add.name} is already in contacts!`);
+      return;
+    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [add, ...contacts],
+    }));
+  };
+
+  deleteContacts = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(
+        (contact) => contact.id !== contactId
+      ),
+    }));
+  };
+
+  changeFilter = (e) => {
+    this.setState({ filter: e.target.value });
+  };
+
+  getvisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    return (
+      <>
+        <div>
+          <h1>Phonebook</h1>
+          <ContactForm onSubmit={this.addContacts} />
+          <h2>Contacts</h2>
+          <Filter value={filter} onChange={this.changeFilter} />
+          <ContactList
+            contacts={this.getvisibleContacts()}
+            onDeleteContacts={this.deleteContacts}
+          />
+        </div>
+      </>
+    );
+  }
+}
+export default App;
