a2e6922aebbb07d6b99bfdd03f11785889531327
Your Name <you@example.com>
2021-07-30 20:15:20 +0300

    hw3.1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import ContactList from "./component/ContactList";
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: "",
   };
 
@@ -48,6 +53,19 @@ class App extends Component {
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { filter } = this.state;
--- src/component/ElContactList/ElContactList.js
+++ src/component/ElContactList/ElContactList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./ElContactList.module.scss";
+
 const ElContactList = ({ contact, onDeleteContacts }) => (
   <>
     <span className={styles.span}>
